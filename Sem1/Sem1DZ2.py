e10b755ddfb0ac3915dce1a5334b9387f7b86465
1286Evgen <lopatingenia@rambler.ru>
2022-09-28 13:14:16 +0300

    end_Sem1DZ2

--- Sem1/Sem1DZ2.py
+++ Sem1/Sem1DZ2.py
@@ -2,3 +2,17 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
 # для всех значений предикат.
 
+trigger = True
+
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            if not(x or y or z) != (not x) and (not y) and (not z):
+                print('выражение не верно')
+                trigger = False
+                break
+
+if trigger:
+    print('выражение верно')
+
+
