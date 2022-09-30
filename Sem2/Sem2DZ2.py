28a433347649a8e529bc1fcc16781c13a5c6472d
1286Evgen <lopatingenia@rambler.ru>
2022-09-30 23:00:02 +0300

    endSem2DZ2

--- Sem2/Sem2DZ2.py
+++ Sem2/Sem2DZ2.py
@@ -1,7 +1,18 @@
+# Sem2DZ2
 # Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N
+# Пример:
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+# N = int(input('введите число: '))
+# result = 1
+# for num in range(1, N):
+#    result *= (num + 1)
+# print(f'факториал числа {N} = {result}')
 
 N = int(input('введите число: '))
-result = 1
-for num in range(1, N):
-    result *= (num + 1)
-print(f'факториал числа {N} = {result}')
+result = []
+res = 1
+for num in range(1, N+1):
+    res = res*num
+    result.append(str(res))
+print('[',', '.join(result), end=' ]')
