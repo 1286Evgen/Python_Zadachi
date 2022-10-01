12da7869c005cc6b14a4bd43f9c58d5d6b177260
1286Evgen <lopatingenia@rambler.ru>
2022-10-01 21:48:11 +0300

    endSem2DZ1

--- Sem2/Sem2DZ1.py
+++ Sem2/Sem2DZ1.py
@@ -6,6 +6,10 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-number = float(input('введите вещественное число: '))
-
-
+number = float(input('введите  число: '))
+strnum1 = str(number)
+strnum2 = strnum1.replace('.','') 
+sumnum = 0
+for i in range(0, len(strnum2)):
+    sumnum = sumnum + int(strnum2[i])
+print(sumnum)
