592ed78011f0875ab9ee3c23eca24d6ea2f9e2da
1286Evgen <lopatingenia@rambler.ru>
2022-10-04 20:59:49 +0300

    endSem3Primer_1

--- Sem3/Sem3Primer_1.py
+++ Sem3/Sem3Primer_1.py
@@ -3,3 +3,15 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов 
 # на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
 
+N = int(input('введите число: '))
+list_N = []
+for num in range(-N, N+1):
+    list_N.append(int(num))
+print(list_N)
+
+with open('numbers.txt') as file:
+    x = int(file.readline())
+    y = int(file.readline())
+print(list_N[x]*list_N[y]) # перемножили 2 и 6 элемент
+
+
