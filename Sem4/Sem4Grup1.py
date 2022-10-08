3c3017bafd25e60d9c6469e13e3cbda351b7c912
1286Evgen <lopatingenia@rambler.ru>
2022-10-08 18:40:23 +0300

    2waySem4Grup1

--- Sem4/Sem4Grup1.py
+++ Sem4/Sem4Grup1.py
@@ -6,20 +6,27 @@
 
 # в одной строке перебираем for -ом то что вводим с клавиатуры, преобразовываем в целое число int, 
 # .split делит строку по пробелам и записывает через запятую
-str_min_max = [int(n) for n in input("введите целые числа: ").split()]
-print(str_min_max)
+#str_min_max = [int(n) for n in input("введите целые числа: ").split()]
+#print(str_min_max)
 
 # первый способ решения:
 
-minima = str_min_max[0]
-maxima = str_min_max[1]
+#minima = str_min_max[0]
+#maxima = str_min_max[1]
 
-for x in str_min_max:
-    if x > maxima:
-        maxima = x
-    elif x < minima:
-        minima = x
+#for x in str_min_max:
+#    if x > maxima:
+#        maxima = x
+#    elif x < minima:
+#        minima = x
+#print(f'минимальное число списка: {minima}, максимальное число списка: {maxima}')
+
+# второй способ решения через функции -min- и -max-:
+
+str_min_max = [int(n) for n in input("введите целые числа: ").split()]
+print(str_min_max)
+
+minima = min(str_min_max)
+maxima = max(str_min_max)
+
 print(f'минимальное число списка: {minima}, максимальное число списка: {maxima}')
-
-# второй способ решения:
-
