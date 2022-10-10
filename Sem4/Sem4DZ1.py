93b2bde765c84bc47fda4c74e05b02be6d735527
1286Evgen <lopatingenia@rambler.ru>
2022-10-10 12:20:49 +0300

    endSem4DZ1

--- Sem4/Sem4DZ1.py
+++ Sem4/Sem4DZ1.py
@@ -6,3 +6,15 @@
 # Пример:
 # - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 
+import math
+
+def num_after_comma(x):
+    res = 0
+    while x % 1 > 0:
+        x *= 10
+        res += 1
+    return res
+
+num_user = float(input("введите необходимую точность при вычислении: "))
+res_pi = (round(math.pi, num_after_comma(num_user)))
+print(res_pi)
