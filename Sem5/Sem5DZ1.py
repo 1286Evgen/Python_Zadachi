e81a5b15c286ec997dcb50a9caacc610b1b40f8d
1286Evgen <lopatingenia@rambler.ru>
2022-10-13 15:46:41 +0300

    endSem5DZ1

---
+++ Sem5/Sem5DZ1.py
@@ -0,0 +1,20 @@
+
+# Sem5DZ1
+
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+text = input("введите текст: ")
+
+split_line = text.split()
+
+new_text = []
+
+for word in split_line:
+    if word.find("абв") == -1:
+        new_text.append(word)
+print("сочетание букв -абв- не содержиться в следующих словах:")
+print(' '.join(new_text))
+
+    
+
+
